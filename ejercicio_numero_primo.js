88999f4361c81ce979217d425e0f2fbbc8dcf7f2
Andres <anondiaz@gmail.com>
2025-03-07 11:28:51 +0100

    dia 2-2

---
+++ ejercicio_numero_primo.js
@@ -0,0 +1,11 @@
+/*
+Dado un número:
+const numero = 12
+
+Indicar si es primo o no
+    -- Si lo es: "El numero X es primo"
+    -- Si no lo es: "El numero X no es primo"
+
+Un numero primo es un número entero que solo es divisible por si mismo y por la unidad
+
+*/
